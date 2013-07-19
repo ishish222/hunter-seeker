2c4987c58844baf4618bff3d655137ebcee65f27
ishish.io <ts@ishish.io>
2013-07-19 12:48:31 +0200

    command dispatching works

--- ps/server.ps1
+++ ps/server.ps1
@@ -1,3 +1,16 @@
+function dispatch-command([string]$command, [system.net.sockets.tcpclient]$client)
+{
+	$ns = $client.getstream()
+	$enc = new-object system.text.asciiEncoding
+	$cmdarray = $command.split(" ")
+	if($cmdarray[0] -eq "test1")
+	{
+		$bytes = $enc.getbytes("odpowiedz na test1\n")
+		$ns.write($bytes, 0, $bytes.length)
+	}
+
+}
+
 function listen-port($port)
 {
 	$enc = new-object system.text.asciiEncoding
@@ -8,19 +21,17 @@ function listen-port($port)
 	$client = new-object system.net.sockets.tcpclient
 	$client = $listener.AcceptTcpClient()
 	"connected"
-#	$ns = system.net.sockets.networkstream
 	$ns = $client.getstream()
 	while($true)
 	{
 		$i = $ns.read($buffer, 0, $buffer.length)
-		$output = $enc.getstring($buffer, 0, $i-1)
-		$output
-		if($output -eq "quit")
+		$command = $enc.getstring($buffer, 0, $i-1)
+		if($command -eq "quit")
 		{
 			break
 		}
+		dispatch-command $command $client
 	}
-#	$ns
 	$listener.stop()
 }
 
