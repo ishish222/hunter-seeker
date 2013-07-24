47ef70cb8df05bfd0032307996afd38a72d4ecc8
ishish.io <ts@ishish.io>
2013-07-24 20:53:18 +0200

    generation worky

--- python/fuzzer.py
+++ python/fuzzer.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 
-import generators
+import generators.generator as generator
+import generators.changer as changer
+
+origin_path = "../origins/acad/test.dwg"
+samples_shared_path = "../samples_shared"
+samples_saved = "../samples_saved"
+
+my_generator = generator.Generator(origin_path, samples_shared_path, ".dwg", changer.Changer)
+
+my_generator.generate(3)
 
 #setup
 
