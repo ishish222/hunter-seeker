50d1c1c449c37e21ceff76e88ee2f071fbe084e2
ishish.io <ts@ishish.io>
2013-07-31 15:15:01 +0200

    Drafting second loop fuzzer

--- python/fuzzer-acad-2nd-loop.py
+++ python/fuzzer-acad-2nd-loop.py
@@ -10,34 +10,49 @@ import logging
 import logging.handlers
 import time
 import sys
+import signal
 
-if(len(sys.argv)) < 3:
-    print("Podaj ip i nazwe boxa")
+visible = False
+Testing = False
+ready = False
+
+class ErrorDetectedException(Exception):
+    pass
+
+if(len(sys.argv)) < 2:
+    print("Podaj nazwe boxa")
     quit()
+
+ips = { 
+'xpsp2-1': '192.168.56.110',
+'fuzzbox-acad-2': '192.168.56.111'
+}
 
 origin_path = "../origins/acad/test.dwg"
 samples_shared_path = "../samples_shared"
 samples_saved = "../samples_saved"
-#fuzzbox_ip = "192.168.56.101"
-fuzzbox_name = sys.argv[2]
-fuzzbox_ip = sys.argv[1]
+fuzzbox_name = sys.argv[1]
+fuzzbox_ip = ips[fuzzbox_name]
 fuzzbox_port = 12345
 buffer_size = 1024
-my_name = "[seeker 1]"
+my_name = "[HS]"
 my_logger = logging.getLogger('MyLogger')
 my_handler = logging.handlers.SysLogHandler(address = '/dev/log')
 my_logger.setLevel(logging.DEBUG)
 my_logger.addHandler(my_handler)
-my_timeout = 10.0
+my_timeout = 20.0
 
-#startvm = ["VBoxManage", "startvm", "", "--type", "headless"]
-startvm = ["VBoxManage", "startvm", ""]
+if(visible):
+    startvm = ["VBoxManage", "startvm", ""]
+else:
+    startvm = ["VBoxManage", "startvm", "", "--type", "headless"]
+
 poweroff = ["VBoxManage", "controlvm", "", "poweroff"]
 restorecurrent = ["VBoxManage", "snapshot", "", "restorecurrent"]
 restorestart = ["VBoxManage", "snapshot", "", "restore", "[x] start"]
 
-def report():
-    my_logger.info(my_name + " " + "reporting");
+def report(string):
+    my_logger.info(my_name + " " + string);
 
 def prepare_fuzzbox():
     pass
@@ -51,23 +66,27 @@ def write_socket(s, data):
     print("> " + str(data))
     s.send(data)
 
-def start():
-    command = startvm
-    command[2] = fuzzbox_name
-    ret = os.spawnv(os.P_WAIT, "/opt/VirtualBox/VBoxManage", command)
-    time.sleep(3)
-
-def restart():
+def powerofff():
     command = poweroff
     command[2] = fuzzbox_name
     ret = os.spawnv(os.P_WAIT, "/opt/VirtualBox/VBoxManage", command)
-    time.sleep(3)
 
+def revert():
     command = restorestart
     command[2] = fuzzbox_name
     ret = os.spawnv(os.P_WAIT, "/opt/VirtualBox/VBoxManage", command)
     time.sleep(3)
 
+def start():
+    revert()
+    command = startvm
+    command[2] = fuzzbox_name
+    ret = os.spawnv(os.P_WAIT, "/opt/VirtualBox/VBoxManage", command)
+    time.sleep(5)
+
+def restart():
+    powerofff()
+    time.sleep(3)
     start()
 
 def connect():
@@ -91,7 +110,7 @@ def init():
 
 def proceed():
     #spawning acad
-    write_socket(s, "spawn calc.exe")
+    write_socket(s, "spawn C:\\Program Files\\AutoCAD 2010\\acad.exe")
     read_socket(s)
 
     #inject to spawned
@@ -101,12 +120,48 @@ def proceed():
     read_socket(s)
     read_socket(s)
 
+    #acad hooks
+    write_socket(s, "pipe installTestHook2")
+    read_socket(s)
+    write_socket(s, "pipe installTestHook3")
+    read_socket(s)
+    write_socket(s, "pipe installTestHook4")
+    read_socket(s)
+    write_socket(s, "pipe installTestHook5")
+    read_socket(s)
+
+    #searching handles
+    write_socket(s, "pipe FindHandles Afx:00400000:b:00010011:00000006")
+    target = read_socket(s)
+    target_class = target.split("-")[1]
+    read_socket(s)
+    print("Setting target: " + str(target_class))
+    
+    #assuming target class is Afx:00400000:b:00010011:00000006:0038052 / changes every spawn?
+    write_socket(s, "pipe SetTargetClass " + str(target_class))
+    read_socket(s)
+
+    #closing windows
+    write_socket(s, "pipe KillClass")
+    read_socket(s)
+
+    print("sleeping")
+    time.sleep(3)
+
     #enter test mode
     write_socket(s, "testmode enter")
     read_socket(s)
     s.settimeout(my_timeout) 
 
-#setup fuzzer
+def sig1_handler(signum, frame):
+    report("Signaled")
+        
+def sigkill_handler(signum, frame):
+    report("Killing")
+    powerofff()
+    quit()
+        
+#setup fuzzer for acad
 my_generator = generator.Generator(origin_path, samples_shared_path, ".dwg", changer.Changer)
 my_generator.mutations=1
 
@@ -116,44 +171,14 @@ connect()
 init()
 proceed()
 
-#until passes test?
+signal.signal(signal.SIGUSR1, sig1_handler)
+signal.signal(signal.SIGINT, sigkill_handler)
+ready = True
+
+report("Second loop ready")
 
 while(True):
-    write_socket(s, "C:\\test.txt")
-    try:
-        read_socket(s)
-        break
-    except socket.timeout:
-        print "timeout/restarting"
-        restart()
-        connect()
-        init()
-        proceed()
-        continue
-
-sample_count = 0
-
-#actual testing
-
-while(True):
-    sample_path = my_generator.generate_one()
-    sample_file = os.path.basename(sample_path)
-    write_socket(s, "Z:\\"+str(sample_file))
-    try:
-        read_socket(s)
-    except socket.timeout:
-        print "timeout/restarting"
-        print "saving" + str(sample_file)
-        restart()
-        init()
-        proceed()
-        continue
-    sample_count = sample_count + 1
-    os.remove(sample_path)
-    if(sample_count % 100 == 0):
-        print("Tested: " + str(sample_count))
-        
-s.settimeout(None)
+    pass
 
 #exit test mode
 write_socket(s, "testmode exit")
